37ea5306dc972ffabe202296739a111799bb074d
Tzu-Mao Li <tzumao@mit.edu>
2018-11-16 15:06:49 -0500

    Assertions for materials

--- pyredner/material.py
+++ pyredner/material.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import pyredner
+import torch
 
 class Material:
     def __init__(self,
@@ -12,15 +13,18 @@ class Material:
                  roughness_uv_scale = torch.tensor([1.0, 1.0]),
                  two_sided = False):
         assert(diffuse_reflectance.is_contiguous())
+        assert(diffuse_reflectance.dtype == torch.float32)
         if specular_reflectance is None:
             specular_reflectance = torch.tensor([0.0,0.0,0.0],
                 device = pyredner.get_device())
         else:
             assert(specular_reflectance.is_contiguous())
+            assert(specular_reflectance.dtype == torch.float32)
         if roughness is None:
             roughness = torch.tensor([1.0], device = pyredner.get_device())
         else:
             assert(roughness.is_contiguous())
+            assert(roughness.dtype == torch.float32)
         if pyredner.get_use_gpu():
             assert(diffuse_reflectance.is_cuda)
             assert(specular_reflectance.is_cuda)
