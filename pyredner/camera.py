9b28b2edfe8ea06d62b56dae51d806b30bcc8654
Tzu-Mao Li <tzumao@mit.edu>
2018-11-16 15:05:19 -0500

    Assertions in Python camera input

--- pyredner/camera.py
+++ pyredner/camera.py
@@ -11,6 +11,16 @@ class Camera:
                  clip_near,
                  resolution,
                  fisheye = False):
+        assert(position.dtype == torch.float32)
+        assert(len(position.shape) == 1 and position.shape[0] == 3)
+        assert(look_at.dtype == torch.float32)
+        assert(len(look_at.shape) == 1 and look_at.shape[0] == 3)
+        assert(up.dtype == torch.float32)
+        assert(len(up.shape) == 1 and up.shape[0] == 3)
+        assert(fov.dtype == torch.float32)
+        assert(len(fov.shape) == 1 and fov.shape[0] == 1)
+        assert(isinstance(clip_near, float))
+
         self.position = position
         self.look_at = look_at
         self.up = up
