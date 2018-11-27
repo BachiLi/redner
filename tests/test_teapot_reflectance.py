Fix mipmapping & ray differential derivatives

tests/test_teapot_reflectance.py
@@ -12,10 +12,11 @@ scene = pyredner.load_mitsuba('scenes/teapot.xml')
 
 # The last material is the teapot material, set it to the target
 scene.materials[-1].diffuse_reflectance = \
-  torch.tensor([0.3, 0.2, 0.2], device = pyredner.get_device())
+    pyredner.Texture(torch.tensor([0.3, 0.2, 0.2], device = pyredner.get_device()))
 scene.materials[-1].specular_reflectance = \
-  torch.tensor([0.6, 0.6, 0.6], device = pyredner.get_device())
-scene.materials[-1].roughness = torch.tensor([0.05], device = pyredner.get_device())
+    pyredner.Texture(torch.tensor([0.6, 0.6, 0.6], device = pyredner.get_device()))
+scene.materials[-1].roughness = \
+    pyredner.Texture(torch.tensor([0.05], device = pyredner.get_device()))
 args = pyredner.RenderFunction.serialize_scene(\
     scene = scene,
     num_samples = 1024,
@@ -35,12 +36,15 @@ if pyredner.get_use_gpu():
 cam = scene.camera
 cam_position = cam.position
 cam_translation = torch.tensor([-0.1, 0.1, -0.1], requires_grad = True)
-scene.materials[-1].diffuse_reflectance = \
-    torch.tensor([0.3, 0.3, 0.3], device = pyredner.get_device(), requires_grad = True)
-scene.materials[-1].specular_reflectance = \
-    torch.tensor([0.5, 0.5, 0.5], device = pyredner.get_device(), requires_grad = True)
-scene.materials[-1].roughness = \
-    torch.tensor([0.2], device = pyredner.get_device(), requires_grad = True)
+diffuse_reflectance = torch.tensor([0.3, 0.3, 0.3],
+    device = pyredner.get_device(), requires_grad = True)
+specular_reflectance = torch.tensor([0.5, 0.5, 0.5],
+    device = pyredner.get_device(), requires_grad = True)
+roughness = torch.tensor([0.2],
+    device = pyredner.get_device(), requires_grad = True)
+scene.materials[-1].diffuse_reflectance = pyredner.Texture(diffuse_reflectance)
+scene.materials[-1].specular_reflectance = pyredner.Texture(specular_reflectance)
+scene.materials[-1].roughness = pyredner.Texture(roughness)
 scene.camera = pyredner.Camera(position     = cam_position + cam_translation,
                                look_at      = cam.look_at,
                                up           = cam.up,
@@ -59,9 +63,9 @@ diff = torch.abs(target - img)
 pyredner.imwrite(diff.cpu(), 'results/test_teapot_reflectance/init_diff.png')
 
 lr = 1e-2
-optimizer = torch.optim.Adam([scene.materials[-1].diffuse_reflectance,
-                              scene.materials[-1].specular_reflectance,
-                              scene.materials[-1].roughness,
+optimizer = torch.optim.Adam([diffuse_reflectance,
+                              specular_reflectance,
+                              roughness,
                               cam_translation], lr=lr)
 num_iteration = 400
 for t in range(num_iteration):
@@ -86,15 +90,15 @@ for t in range(num_iteration):
     print('loss:', loss.item())
 
     loss.backward()
-    print('diffuse_reflectance.grad:', scene.materials[-1].diffuse_reflectance.grad)
-    print('specular_reflectance.grad:', scene.materials[-1].specular_reflectance.grad)
-    print('roughness.grad:', scene.materials[-1].roughness.grad)
+    print('diffuse_reflectance.grad:', diffuse_reflectance.grad)
+    print('specular_reflectance.grad:', specular_reflectance.grad)
+    print('roughness.grad:', roughness.grad)
     print('cam_translation.grad:', cam_translation.grad)
 
     optimizer.step()
-    print('diffuse_reflectance:', scene.materials[-1].diffuse_reflectance)
-    print('specular_reflectance:', scene.materials[-1].specular_reflectance)
-    print('roughness:', scene.materials[-1].roughness)
+    print('diffuse_reflectance:', diffuse_reflectance)
+    print('specular_reflectance:', specular_reflectance)
+    print('roughness:', roughness)
     print('cam_translation:', cam_translation)
 
     # Linearly reduce the learning rate

tests/test_teapot_specular.py
@@ -12,11 +12,11 @@ scene = pyredner.load_mitsuba('scenes/teapot_specular.xml')
 
 # The last material is the teapot material, set it to a specular material
 scene.materials[-1].diffuse_reflectance = \
-    torch.tensor([0.15, 0.2, 0.15], device = pyredner.get_device())
+    pyredner.Texture(torch.tensor([0.15, 0.2, 0.15], device = pyredner.get_device()))
 scene.materials[-1].specular_reflectance = \
-    torch.tensor([0.8, 0.8, 0.8], device = pyredner.get_device())
+    pyredner.Texture(torch.tensor([0.8, 0.8, 0.8], device = pyredner.get_device()))
 scene.materials[-1].roughness = \
-    torch.tensor([0.0001], device = pyredner.get_device())
+    pyredner.Texture(torch.tensor([0.0001], device = pyredner.get_device()))
 args=pyredner.RenderFunction.serialize_scene(\
     scene = scene,
     num_samples = 512,