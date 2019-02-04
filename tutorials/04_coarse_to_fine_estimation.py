2668fcc0bac4b8ea73bb4760066a84842557f8ea
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2019-02-04 10:34:30 -0500

    finetune tutorial

--- tutorials/04_coarse_to_fine_estimation.py
+++ tutorials/04_coarse_to_fine_estimation.py
@@ -43,15 +43,13 @@ for mi, m in enumerate(scene.materials):
     material_vars.append(var)
     m.diffuse_reflectance = pyredner.Texture(var)
         
-# And let's also slightly perturb the camera pose a bit
-position = torch.tensor([275., 275., -805.], requires_grad = True)
-look_at = torch.tensor([275., 275., 0.], requires_grad = True)
-up = torch.tensor([0.1, 0.9, -0.1], requires_grad = True)
-fov = torch.tensor([40.0], requires_grad = True)
-cam_vars = [position, look_at, up, fov]
+# And let's also slightly perturb the camera up vector and field of view a bit
+up = torch.tensor([0.2, 0.8, -0.2], requires_grad = True)
+fov = torch.tensor([41.0], requires_grad = True)
+cam_vars = [up, fov]
 scene.camera = pyredner.Camera(\
-    position = position,
-    look_at = look_at,
+    position = scene.camera.position,
+    look_at = scene.camera.look_at,
     up = up,
     fov = fov,
     clip_near = scene.camera.clip_near,
@@ -67,11 +65,14 @@ pyredner.imwrite(img.cpu(), 'results/coarse_to_fine_estimation/init.png')
 # Optimize for parameters.
 optimizer = torch.optim.Adam(material_vars + cam_vars, lr=5e-3)
 # We run a coarse-to-fine estimation here to prevent being trapped in local minimum
-# The final resolution is 256x256, but we will start from an 128x128 image
-res = [128, 256]
-iters = [100, 100]
+# The final resolution is 256x256, but we will start from an 64x64 image
+# We can also tweak the number of iterations, samples per pixel, number of bounces,
+# learning rate for each resolution
+res = [64, 256]
+iters = [400, 200]
 spp = [16, 4]
 bounces = [5, 5]
+lrs = [5e-3, 1e-3]
 iter_count = 0
 for ri, r in enumerate(res):
     scene.camera.resolution = (r, r)
@@ -85,14 +86,17 @@ for ri, r in enumerate(res):
     if pyredner.get_use_gpu():
         resampled_target = resampled_target.cuda()
 
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lrs[ri]
+
     for t in range(iters[ri]):
         print('resolution:', r, ', iteration:', iter_count)
         optimizer.zero_grad()
         # Forward pass: serialize the scene and render the image
         # Need to redefine the camera
         scene.camera = pyredner.Camera(\
-            position = position,
-            look_at = look_at,
+            position = scene.camera.position,
+            look_at = scene.camera.look_at,
             up = up,
             fov = fov,
             clip_near = scene.camera.clip_near,
@@ -100,14 +104,14 @@ for ri, r in enumerate(res):
         scene_args = pyredner.RenderFunction.serialize_scene(\
             scene = scene,
             num_samples = spp[ri],
-            max_bounces = bounces[ri]) # increase number of bounces over time
+            max_bounces = bounces[ri])
         # Important to use a different seed every iteration, otherwise the result
         # would be biased.
         img = render(iter_count+1, *scene_args)
         # Save the intermediate render.
         img_np = img.data.cpu().numpy()
         img_np = scipy.ndimage.interpolation.zoom(\
-            img_np, (512.0/r, 512.0/r, 1.0),
+            img_np, (256.0/r, 256.0/r, 1.0),
             order=1)
         pyredner.imwrite(torch.from_numpy(img_np),
             'results/coarse_to_fine_estimation/iter_{}.png'.format(iter_count))
@@ -138,10 +142,12 @@ for ri, r in enumerate(res):
         diff_1 = m(torch.nn.functional.conv2d(diff_0, f, padding=3))
         diff_2 = m(torch.nn.functional.conv2d(diff_1, f, padding=3))
         diff_3 = m(torch.nn.functional.conv2d(diff_2, f, padding=3))
+        diff_4 = m(torch.nn.functional.conv2d(diff_3, f, padding=3))
         loss = diff_0.pow(2).sum() / (r * r) + \
                diff_1.pow(2).sum() / ((r/2.)*(r/2.)) + \
                diff_2.pow(2).sum() / ((r/4.)*(r/4.)) + \
-               diff_3.pow(2).sum() / ((r/8.)*(r/8.))
+               diff_3.pow(2).sum() / ((r/8.)*(r/8.)) + \
+               diff_4.pow(2).sum() / ((r/16.)*(r/16.))
         print('loss:', loss.item())
 
         # Backpropagate the gradients.
@@ -149,8 +155,8 @@ for ri, r in enumerate(res):
 
         # Take a gradient descent step.
         optimizer.step()
-        print('position:', position)
         print('up:', up)
+        print('fov:', fov)
 
         # Important: the material parameters has hard constraints: the
         # reflectance and roughness cannot be negative. We enforce them here
@@ -161,8 +167,8 @@ for ri, r in enumerate(res):
 
 # Render the final result.
 scene.camera = pyredner.Camera(\
-    position = position,
-    look_at = look_at,
+    position = scene.camera.position,
+    look_at = scene.camera.look_at,
     up = up,
     fov = fov,
     clip_near = scene.camera.clip_near,
@@ -170,8 +176,8 @@ scene.camera = pyredner.Camera(\
 scene_args = pyredner.RenderFunction.serialize_scene(\
     scene = scene,
     num_samples = 512,
-    max_bounces = 6)
-img = render(202, *scene_args)
+    max_bounces = 5)
+img = render(402, *scene_args)
 # Save the images and differences.
 pyredner.imwrite(img.cpu(), 'results/coarse_to_fine_estimation/final.exr')
 pyredner.imwrite(img.cpu(), 'results/coarse_to_fine_estimation/final.png')
