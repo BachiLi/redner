8ed2522ef4fb7da4f7ae076692025244a6cc9935
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2019-02-10 14:10:28 -0500

    Fix a memory bug

--- camera.cpp
+++ camera.cpp
@@ -68,7 +68,7 @@ void accumulate_camera(const DCameraInst &d_camera_inst,
     }
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            d_camera.cam_to_ndc[4 * i + j] += d_camera_inst.cam_to_ndc(i, j);
+            d_camera.cam_to_ndc[3 * i + j] += d_camera_inst.cam_to_ndc(i, j);
         }
     }
 }
