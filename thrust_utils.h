70dbb129519807e5875224a361b5a23069a0769f
Tzu-Mao Li <tzumao@mit.edu>
2019-02-12 17:25:40 -0500

    fix thrust allocator

--- thrust_utils.h
+++ thrust_utils.h
@@ -22,58 +22,78 @@
 struct ThrustCachedAllocator {
     using value_type = char;
 
+    static constexpr int max_num_buffers = 2;
+    int num_buffers;
     bool use_gpu;
-    int block_size;
-    char* block_ptr;
-    bool used;
+    int block_size[max_num_buffers];
+    char* block_ptr[max_num_buffers];
+    bool used[max_num_buffers];
 
     ThrustCachedAllocator(bool use_gpu, int init_block_size = 0) : use_gpu(use_gpu) {
-        block_size = init_block_size;
-        block_ptr = nullptr;
-        if (block_size > 0) {
-            if (use_gpu) {
-                block_ptr = thrust::device_malloc<char>(block_size).get();
-            } else {
-                block_ptr = new char[block_size];
+        num_buffers = use_gpu ? 1 : 2;
+        for (int i = 0; i < num_buffers; i++) {
+            block_size[i] = init_block_size;
+            block_ptr[i] = nullptr;
+            if (block_size[i] > 0) {
+                if (use_gpu) {
+                    block_ptr[i] = thrust::device_malloc<char>(block_size[i]).get();
+                } else {
+                    block_ptr[i] = new char[block_size[i]];
+                }
             }
+            used[i] = false;
         }
-        used = false;
     }
 
     ~ThrustCachedAllocator() {
-        if (block_ptr != nullptr) {
-            if (use_gpu) {
-                thrust::device_free(thrust::device_ptr<char>(block_ptr));
-            } else {
-                delete[] block_ptr;
+        for (int i = 0; i < num_buffers; i++) {
+            if (block_ptr[i] != nullptr) {
+                if (use_gpu) {
+                    thrust::device_free(thrust::device_ptr<char>(block_ptr[i]));
+                } else {
+                    delete[] block_ptr[i];
+                }
             }
         }
     }
 
     char* allocate(std::ptrdiff_t num_bytes) {
-        assert(!used);
-        if (num_bytes > block_size) {
-            if (block_ptr != nullptr) {
+        for (int i = 0; i < num_buffers; i++) {
+            // Find first unused buffer
+            if (used[i]) {
+                continue;
+            }
+            if (num_bytes > block_size[i]) {
+                if (block_ptr[i] != nullptr) {
+                    if (use_gpu) {
+                        thrust::device_free(thrust::device_ptr<char>(block_ptr[i]));
+                    } else {
+                        delete[] block_ptr[i];
+                    }
+                }
+                block_size[i] = num_bytes;
                 if (use_gpu) {
-                    thrust::device_free(thrust::device_ptr<char>(block_ptr));
+                    block_ptr[i] = thrust::device_malloc<char>(block_size[i]).get();
                 } else {
-                    delete[] block_ptr;
+                    block_ptr[i] = new char[block_size[i]];
                 }
             }
-            block_size = num_bytes;
-            if (use_gpu) {
-                block_ptr = thrust::device_malloc<char>(block_size).get();
-            } else {
-                block_ptr = new char[block_size];
-            }
+            used[i] = true;
+            return block_ptr[i];
         }
-        used = true;
-        return block_ptr;
+        assert(false);
+        return nullptr;
     }
 
     void deallocate(char* ptr, size_t n) {
-        assert(used);
-        used = false;
+        for (int i = 0; i < num_buffers; i++) {
+            if (block_ptr[i] == ptr) {
+                assert(used[i]);
+                used[i] = false;
+                return;
+            }
+        }
+        assert(false);
     }
 };
 
