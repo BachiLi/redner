58ffc474a7abe7ffb047c7c32855b5c456fa046f
Tzu-Mao Li <tzumao@tefnut.csail.mit.edu>
2019-05-02 19:06:01 -0400

    Fix Embree cmake build

--- cmake/FindEmbree.cmake
+++ cmake/FindEmbree.cmake
@@ -1,9 +1,11 @@
 find_path(EMBREE_INCLUDE_PATH embree3/rtcore.h
+  ${PROJECT_SOURCE_DIR}/embree/include
   /usr/include
   /usr/local/include
   /opt/local/include)
 
 find_library(EMBREE_LIBRARY NAMES embree3 PATHS 
+  ${PROJECT_SOURCE_DIR}/embree/build
   /usr/lib 
   /usr/local/lib 
   /opt/local/lib)
