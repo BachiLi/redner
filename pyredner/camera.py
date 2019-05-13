Fix camera backprop & living room scene

pyredner/camera.py
@@ -44,12 +44,12 @@ class Camera:
             assert(len(fov.shape) == 1 and fov.shape[0] == 1)
         assert(isinstance(clip_near, float))
 
-        self._position = position
-        self._look_at = look_at
-        self._up = up
-        self._fov = fov
-        self.cam_to_world = transform.gen_look_at_matrix(position, look_at, up)
-        self.world_to_cam = torch.inverse(self.cam_to_world).contiguous()
+        self.position = position
+        self.look_at = look_at
+        self.up = up
+        self.fov = fov
+        # self.cam_to_world = transform.gen_look_at_matrix(position, look_at, up)
+        # self.world_to_cam = torch.inverse(self.cam_to_world).contiguous()
         if cam_to_ndc is None:
             fov_factor = 1.0 / torch.tan(transform.radians(0.5 * fov))
             o = torch.ones([1], dtype=torch.float32)
@@ -61,39 +61,6 @@ class Camera:
         self.clip_near = clip_near
         self.resolution = resolution
         self.fisheye = fisheye
-
-    @property
-    def position(self):
-        return self._position
-
-    @position.setter
-    def position(self, value):
-        self._position = value
-        self.cam_to_world = \
-            transform.gen_look_at_matrix(self._position, self._look_at, self._up)
-        self.world_to_cam = torch.inverse(self.cam_to_world).contiguous()
-
-    @property
-    def look_at(self):
-        return self._look_at
-    
-    @look_at.setter
-    def look_at(self, value):
-        self._look_at = value
-        self.cam_to_world = \
-            transform.gen_look_at_matrix(self._position, self._look_at, self._up)
-        self.world_to_cam = torch.inverse(self.cam_to_world).contiguous()
-
-    @property
-    def up(self):
-        return self._up
-
-    @up.setter
-    def up(self, value):
-        self._up = value
-        self.cam_to_world = \
-            transform.gen_look_at_matrix(self._position, self._look_at, self._up)
-        self.world_to_cam = torch.inverse(self.cam_to_world).contiguous()
 
     @property
     def fov(self):
@@ -123,8 +90,6 @@ class Camera:
             'look_at': self._look_at,
             'up': self._up,
             'fov': self._fov,
-            'cam_to_world': self.cam_to_world,
-            'world_to_cam': self.world_to_cam,
             'cam_to_ndc': self._cam_to_ndc,
             'ndc_to_cam': self.ndc_to_cam,
             'clip_near': self.clip_near,
@@ -139,8 +104,6 @@ class Camera:
         out._look_at = state_dict['look_at']
         out._up = state_dict['up']
         out._fov = state_dict['fov']
-        out.cam_to_world = state_dict['cam_to_world']
-        out.world_to_cam = state_dict['world_to_cam']
         out._cam_to_ndc = state_dict['cam_to_ndc']
         out.ndc_to_cam = state_dict['ndc_to_cam']
         out.clip_near = state_dict['clip_near']

pyredner/render_pytorch.py
@@ -77,8 +77,9 @@ class RenderFunction(torch.autograd.Function):
         args.append(num_shapes)
         args.append(num_materials)
         args.append(num_lights)
-        args.append(cam.cam_to_world)
-        args.append(cam.world_to_cam)
+        args.append(cam.position)
+        args.append(cam.look_at)
+        args.append(cam.up)
         args.append(cam.ndc_to_cam)
         args.append(cam.cam_to_ndc)
         args.append(cam.clip_near)
@@ -141,9 +142,11 @@ class RenderFunction(torch.autograd.Function):
         current_index += 1
         num_lights = args[current_index]
         current_index += 1
-        cam_to_world = args[current_index]
+        cam_position = args[current_index]
         current_index += 1
-        world_to_cam = args[current_index]
+        cam_look_at = args[current_index]
+        current_index += 1
+        cam_up = args[current_index]
         current_index += 1
         ndc_to_cam = args[current_index]
         current_index += 1
@@ -155,12 +158,11 @@ class RenderFunction(torch.autograd.Function):
         current_index += 1
         fisheye = args[current_index]
         current_index += 1
-        assert(cam_to_world.is_contiguous())
-        assert(world_to_cam.is_contiguous())
         camera = redner.Camera(resolution[1],
                                resolution[0],
-                               redner.float_ptr(cam_to_world.data_ptr()),
-                               redner.float_ptr(world_to_cam.data_ptr()),
+                               redner.float_ptr(cam_position.data_ptr()),
+                               redner.float_ptr(cam_look_at.data_ptr()),
+                               redner.float_ptr(cam_up.data_ptr()),
                                redner.float_ptr(ndc_to_cam.data_ptr()),
                                redner.float_ptr(cam_to_ndc.data_ptr()),
                                clip_near,
@@ -367,12 +369,14 @@ class RenderFunction(torch.autograd.Function):
         scene = ctx.scene
         options = ctx.options
 
-        d_cam_to_world = torch.zeros(4, 4)
-        d_world_to_cam = torch.zeros(4, 4)
+        d_cam_position = torch.zeros(3)
+        d_cam_look = torch.zeros(3)
+        d_cam_up = torch.zeros(3)
         d_ndc_to_cam = torch.zeros(3, 3)
         d_cam_to_ndc = torch.zeros(3, 3)
-        d_camera = redner.DCamera(redner.float_ptr(d_cam_to_world.data_ptr()),
-                                  redner.float_ptr(d_world_to_cam.data_ptr()),
+        d_camera = redner.DCamera(redner.float_ptr(d_cam_position.data_ptr()),
+                                  redner.float_ptr(d_cam_look.data_ptr()),
+                                  redner.float_ptr(d_cam_up.data_ptr()),
                                   redner.float_ptr(d_ndc_to_cam.data_ptr()),
                                   redner.float_ptr(d_cam_to_ndc.data_ptr()))
         d_vertices_list = []
@@ -497,7 +501,7 @@ class RenderFunction(torch.autograd.Function):
 
         # # For debugging
         # # pyredner.imwrite(grad_img, 'grad_img.exr')
-        # grad_img = torch.ones(256, 256, 3, device = pyredner.get_device())
+        # # grad_img = torch.ones(256, 256, 3, device = pyredner.get_device())
         # debug_img = torch.zeros(256, 256, 3)
         # start = time.time()
         # redner.render(scene, options,
@@ -512,7 +516,10 @@ class RenderFunction(torch.autograd.Function):
         # pyredner.imwrite(-debug_img, 'debug_.exr')
         # debug_img = debug_img.numpy()
         # print(np.max(debug_img))
+        # print(np.unravel_index(np.argmax(debug_img), debug_img.shape))
         # print(np.min(debug_img))
+        # print(np.unravel_index(np.argmin(debug_img), debug_img.shape))
+        # print(np.sum(debug_img) / 3)
         # debug_max = 0.5
         # debug_min = -0.5
         # debug_img = np.clip((debug_img - debug_min) / (debug_max - debug_min), 0, 1)
@@ -527,8 +534,9 @@ class RenderFunction(torch.autograd.Function):
         ret_list.append(None) # num_shapes
         ret_list.append(None) # num_materials
         ret_list.append(None) # num_lights
-        ret_list.append(d_cam_to_world)
-        ret_list.append(d_world_to_cam)
+        ret_list.append(d_cam_position)
+        ret_list.append(d_cam_look)
+        ret_list.append(d_cam_up)
         ret_list.append(d_ndc_to_cam)
         ret_list.append(d_cam_to_ndc)
         ret_list.append(None) # clip near