ee62afc9b7bd7312b2f9702dc7c7342907544fbe
Tzu-Mao Li <tzumao@tefnut.csail.mit.edu>
2019-05-14 11:17:20 -0400

    make teapot reflectance example easier

--- tests/test_teapot_reflectance.py
+++ tests/test_teapot_reflectance.py
@@ -1,6 +1,7 @@
 import pyredner
 import numpy as np
 import torch
+import scipy
 
 # Optimize for material parameters and camera pose
 
@@ -35,7 +36,7 @@ if pyredner.get_use_gpu():
 # Perturb the scene, this is our initial guess
 cam = scene.camera
 cam_position = cam.position
-cam_translation = torch.tensor([-0.1, 0.1, -0.1], requires_grad = True)
+cam_translation = torch.tensor([-0.2, 0.2, -0.2], requires_grad = True)
 diffuse_reflectance = torch.tensor([0.3, 0.3, 0.3],
     device = pyredner.get_device(), requires_grad = True)
 specular_reflectance = torch.tensor([0.5, 0.5, 0.5],
@@ -46,7 +47,7 @@ scene.materials[-1].diffuse_reflectance = pyredner.Texture(diffuse_reflectance)
 scene.materials[-1].specular_reflectance = pyredner.Texture(specular_reflectance)
 scene.materials[-1].roughness = pyredner.Texture(roughness)
 scene.camera = pyredner.Camera(position     = cam_position + cam_translation,
-                               look_at      = cam.look_at,
+                               look_at      = cam.look_at + cam_translation,
                                up           = cam.up,
                                fov          = cam.fov,
                                clip_near    = cam.clip_near,
@@ -62,7 +63,8 @@ pyredner.imwrite(img.cpu(), 'results/test_teapot_reflectance/init.png')
 diff = torch.abs(target - img)
 pyredner.imwrite(diff.cpu(), 'results/test_teapot_reflectance/init_diff.png')
 
-lr = 1e-2
+lr_base = 1e-2
+lr = lr_base
 optimizer = torch.optim.Adam([diffuse_reflectance,
                               specular_reflectance,
                               roughness,
@@ -74,7 +76,7 @@ for t in range(num_iteration):
     # Forward pass: render the image
     # need to rerun Camera constructor for autodiff 
     scene.camera = pyredner.Camera(position   = cam_position + cam_translation,
-                                   look_at    = cam.look_at,
+                                   look_at    = cam.look_at + cam_translation,
                                    up         = cam.up,
                                    fov        = cam.fov,
                                    clip_near  = cam.clip_near,
@@ -86,7 +88,35 @@ for t in range(num_iteration):
         max_bounces = 2)
     img = render(t+1, *args)
     pyredner.imwrite(img.cpu(), 'results/test_teapot_reflectance/iter_{}.png'.format(t))
-    loss = (img - target).pow(2).sum()
+    #loss = (img - target).pow(2).sum()
+
+    diff = img - target
+    dirac = np.zeros([7,7], dtype = np.float32)
+    dirac[3,3] = 1.0
+    dirac = torch.from_numpy(dirac)
+    f = np.zeros([3, 3, 7, 7], dtype = np.float32)
+    gf = scipy.ndimage.filters.gaussian_filter(dirac, 1.0)
+    f[0, 0, :, :] = gf
+    f[1, 1, :, :] = gf
+    f[2, 2, :, :] = gf
+    f = torch.from_numpy(f)
+    if pyredner.get_use_gpu():
+        f = f.cuda(device = pyredner.get_device())
+    m = torch.nn.AvgPool2d(2)
+    r = 256
+    diff_0 = (img - target).view(1, r, r, 3).permute(0, 3, 2, 1)
+    diff_1 = m(torch.nn.functional.conv2d(diff_0, f, padding=3))
+    diff_2 = m(torch.nn.functional.conv2d(diff_1, f, padding=3))
+    diff_3 = m(torch.nn.functional.conv2d(diff_2, f, padding=3))
+    diff_4 = m(torch.nn.functional.conv2d(diff_3, f, padding=3))
+    diff_5 = m(torch.nn.functional.conv2d(diff_4, f, padding=3))
+    loss = diff_0.pow(2).sum() / (r*r) + \
+           diff_1.pow(2).sum() / ((r/2)*(r/2)) + \
+           diff_2.pow(2).sum() / ((r/4)*(r/4)) + \
+           diff_3.pow(2).sum() / ((r/8)*(r/8)) + \
+           diff_4.pow(2).sum() / ((r/16)*(r/16)) + \
+           diff_5.pow(2).sum() / ((r/32)*(r/32))
+
     print('loss:', loss.item())
 
     loss.backward()
@@ -96,19 +126,20 @@ for t in range(num_iteration):
     print('cam_translation.grad:', cam_translation.grad)
 
     # HACK: gradient clipping to deal with outlier gradients
-    torch.nn.utils.clip_grad_norm_(roughness, 10000)
-    torch.nn.utils.clip_grad_norm_(cam_translation, 10000)
+    torch.nn.utils.clip_grad_norm_(roughness, 10)
+    torch.nn.utils.clip_grad_norm_(cam_translation, 10)
 
     optimizer.step()
+
     print('diffuse_reflectance:', diffuse_reflectance)
     print('specular_reflectance:', specular_reflectance)
     print('roughness:', roughness)
     print('cam_translation:', cam_translation)
 
     # Linearly reduce the learning rate
-    lr = 1e-2 * float(num_iteration - t) / float(num_iteration)
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+    #lr = lr_base * float(num_iteration - t) / float(num_iteration)
+    #for param_group in optimizer.param_groups:
+    #    param_group['lr'] = lr
 
 args = pyredner.RenderFunction.serialize_scene(\
     scene = scene,
