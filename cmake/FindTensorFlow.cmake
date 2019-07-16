a1d68e66ec9ea554014761d019267919fb518751
Tzu-Mao Li <tzumao@mit.edu>
2019-07-16 08:59:41 -0700

    Fix tensorflow cmake

--- cmake/FindTensorFlow.cmake
+++ cmake/FindTensorFlow.cmake
@@ -1,25 +1,21 @@
 # https://github.com/PatWie/tensorflow-cmake/blob/master/cmake/modules/FindTensorFlow.cmake
 
-if(APPLE)
-    execute_process(
-      COMMAND python -c "import tensorflow as tf; print(tf.__version__); print(tf.__cxx11_abi_flag__); print(tf.sysconfig.get_include()); print(tf.sysconfig.get_lib() + '/libtensorflow_framework.dylib')"
-      OUTPUT_VARIABLE TF_INFORMATION_STRING
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      RESULT_VARIABLE retcode)
-else()
-  execute_process(
-      COMMAND python -c "import tensorflow as tf; print(tf.__version__); print(tf.__cxx11_abi_flag__); print(tf.sysconfig.get_include()); print(tf.sysconfig.get_lib() + '/libtensorflow_framework.so')"
-      OUTPUT_VARIABLE TF_INFORMATION_STRING
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      RESULT_VARIABLE retcode)
-endif()
+execute_process(
+    COMMAND python -c "import tensorflow as tf; print(tf.__version__); print(tf.__cxx11_abi_flag__); print(tf.sysconfig.get_include()); print(tf.sysconfig.get_lib())"
+    OUTPUT_VARIABLE TF_INFORMATION_STRING
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    RESULT_VARIABLE retcode)
 
 if("${retcode}" STREQUAL "0")
     string(REPLACE "\n" ";" TF_INFORMATION_LIST ${TF_INFORMATION_STRING})
     list(GET TF_INFORMATION_LIST 0 TF_DETECTED_VERSION)
     list(GET TF_INFORMATION_LIST 1 TF_DETECTED_ABI)
     list(GET TF_INFORMATION_LIST 2 TF_DETECTED_INCLUDE_DIR)
-    list(GET TF_INFORMATION_LIST 3 TF_DETECTED_LIBRARY)
+    list(GET TF_INFORMATION_LIST 3 TF_DETECTED_LIBRARY_DIR)
+    message(STATUS ${TF_DETECTED_LIBRARY_DIR})
+    find_library(TF_DETECTED_LIBRARY NAMES tensorflow_framework PATHS 
+        ${TF_DETECTED_LIBRARY_DIR})
+    message(STATUS ${TF_DETECTED_LIBRARY})
     set(TensorFlow_VERSION ${TF_DETECTED_VERSION})
     set(TensorFlow_ABI ${TF_DETECTED_ABI})
     set(TensorFlow_INCLUDE_DIR ${TF_DETECTED_INCLUDE_DIR})
--- tests_tensorflow/test_scatter_add.py
+++ tests_tensorflow/test_scatter_add.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
 tf.enable_eager_execution()
-import pyrednertensorflow as pyredner
+import pyrednertensorflow as pyrednertf
 import pyredner as pyrednertorch
 
 import numpy as np
@@ -11,10 +11,10 @@ class TestScatterAdd(tf.test.TestCase):
 
     def testComputeVertexNormal(self):
         _, mesh_list_torch, _ = pyrednertorch.load_obj('scenes/teapot.obj')
-        _, mesh_list_tf, _ = pyredner.load_obj('scenes/teapot.obj')
+        _, mesh_list_tf, _ = pyrednertf.load_obj('scenes/teapot.obj')
 
         for (_, mesh1), (_ ,mesh2) in zip(mesh_list_tf, mesh_list_torch):
-            normalstf = pyredner.compute_vertex_normal(mesh1.vertices, mesh1.indices)
+            normalstf = pyrednertf.compute_vertex_normal(mesh1.vertices, mesh1.indices)
             normalstorch = pyrednertorch.compute_vertex_normal(mesh2.vertices, mesh2.indices)
             self.assertTrue(
                 np.allclose(normalstf.numpy(), normalstorch.detach().numpy(), atol=1e-07)
@@ -22,4 +22,3 @@ class TestScatterAdd(tf.test.TestCase):
 
 if __name__ == "__main__":
     tf.test.main()
-
