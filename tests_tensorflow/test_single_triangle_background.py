b3f1249a0db8ff1d80e74d1ed560ee095168b0b4
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2019-07-26 16:57:54 -0400

    rewrite test_single_triangle_background

--- tests_tensorflow/test_single_triangle_background.py
+++ tests_tensorflow/test_single_triangle_background.py
@@ -1,57 +1,58 @@
 import tensorflow as tf
-tf.enable_eager_execution()
-tfe = tf.contrib.eager
+tf.compat.v1.enable_eager_execution()
 import pyrednertensorflow as pyredner
-import redner
-import numpy as np
 import skimage.transform
+import redner
 
 # Optimize three vertices of a single triangle, with a SIGGRAPH logo background
 
 # Use GPU if available
-pyredner.set_use_gpu(False)
+pyredner.set_use_gpu(tf.test.is_gpu_available(cuda_only=True, min_cuda_compute_capability=None))
 
 # Set up the pyredner scene for rendering:
 
 # Setup camera
-cam = pyredner.Camera(position = tfe.Variable([0.0, 0.0, -5.0], dtype=tf.float32),
-                      look_at = tfe.Variable([0.0, 0.0, 0.0], dtype=tf.float32),
-                      up = tfe.Variable([0.0, 1.0, 0.0], dtype=tf.float32),
-                      fov = tfe.Variable([45.0], dtype=tf.float32), # in degree
-                      clip_near = 1e-2, # needs to > 0
-                      resolution = (256, 256),
-                      fisheye = False)
+with tf.device('/device:cpu:' + str(pyredner.get_cpu_device_id())):
+    cam = pyredner.Camera(position = tf.Variable([0.0, 0.0, -5.0], dtype=tf.float32, use_resource=True),
+                          look_at = tf.Variable([0.0, 0.0, 0.0], dtype=tf.float32, use_resource=True),
+                          up = tf.Variable([0.0, 1.0, 0.0], dtype=tf.float32, use_resource=True),
+                          fov = tf.Variable([45.0], dtype=tf.float32, use_resource=True), # in degree
+                          clip_near = 1e-2, # needs to > 0
+                          resolution = (256, 256),
+                          fisheye = False)
 
 # Setup materials
-mat_grey = pyredner.Material(
-    diffuse_reflectance = \
-        tfe.Variable([0.5, 0.5, 0.5], dtype=tf.float32))
-# The material list of the scene
-materials = [mat_grey]
+with tf.device(pyredner.get_device_name()):
+    mat_grey = pyredner.Material(
+        diffuse_reflectance = tf.Variable([0.5, 0.5, 0.5], dtype=tf.float32, use_resource=True))
+    # The material list of the scene
+    materials = [mat_grey]
 
-# Setup geometries
-shape_triangle = pyredner.Shape(
-    vertices = tfe.Variable([[-1.7, 1.0, 0.0], [1.0, 1.0, 0.0], [-0.5, -1.0, 0.0]], dtype=tf.float32),
-    indices = tfe.Variable([[0, 1, 2]], dtype=tf.int32),
-    uvs = None,
-    normals = None,
-    material_id = 0)
-# Setup light source shape
-shape_light = pyredner.Shape(
-    vertices = tfe.Variable([[-1.0, -1.0, -7.0],
-                             [ 1.0, -1.0, -7.0],
-                             [-1.0,  1.0, -7.0],
-                             [ 1.0,  1.0, -7.0]], dtype=tf.float32),
-    indices = tfe.Variable([[0, 1, 2],[1, 3, 2]], dtype=tf.int32),
-    uvs = None,
-    normals = None,
-    material_id = 0)
+    # Setup geometries
+    shape_triangle = pyredner.Shape(
+        vertices = tf.Variable([[-1.7, 1.0, 0.0], [1.0, 1.0, 0.0], [-0.5, -1.0, 0.0]],
+            dtype=tf.float32, use_resource=True),
+        indices = tf.constant([[0, 1, 2]], dtype=tf.int32),
+        uvs = None,
+        normals = None,
+        material_id = 0)
+    # Setup light source shape
+    shape_light = pyredner.Shape(
+        vertices = tf.Variable([[-1.0, -1.0, -7.0],
+                                [ 1.0, -1.0, -7.0],
+                                [-1.0,  1.0, -7.0],
+                                [ 1.0,  1.0, -7.0]], dtype=tf.float32, use_resource=True),
+        indices = tf.constant([[0, 1, 2],[1, 3, 2]], dtype=tf.int32),
+        uvs = None,
+        normals = None,
+        material_id = 0)
 # The shape list of the scene
 shapes = [shape_triangle, shape_light]
 
 # Setup light source
-light = pyredner.AreaLight(shape_id = 1, 
-                           intensity = tfe.Variable([20.0,20.0,20.0], dtype=tf.float32))
+with tf.device('/device:cpu:' + str(pyredner.get_cpu_device_id())):
+    light = pyredner.AreaLight(shape_id = 1, 
+                               intensity = tf.Variable([20.0,20.0,20.0], dtype=tf.float32, use_resource=True))
 area_lights = [light]
 
 # Construct the scene
@@ -80,50 +81,46 @@ pyredner.imwrite(img, 'results/test_single_triangle_background/target.png')
 # Read the target image we just saved.
 target = pyredner.imread('results/test_single_triangle_background/target.exr')
 
-DEBUG = False
-if not DEBUG:
-    """
-    NOTE: Pass channel! It's different from test_single_triangle.py 
-    """
-    scene_args = pyredner.serialize_scene(
-        scene = scene,
-        num_samples = 16,
-        max_bounces = 1,
-        channels = [redner.channels.radiance, redner.channels.alpha])
+scene_args = pyredner.serialize_scene(
+    scene = scene,
+    num_samples = 16,
+    max_bounces = 1,
+    channels = [redner.channels.radiance, redner.channels.alpha])
 
-    # Render. The first argument is the seed for RNG in the renderer.
-    img = pyredner.render(0, *scene_args)
-    # Since we specified alpha as output channel, img has 4 channels now
-    # We blend the image with a background image
-    img = img[:, :, :3] * img[:, :, 3:4] + background * (1 - img[:, :, 3:4])
+# Render. The first argument is the seed for RNG in the renderer.
+img = pyredner.render(0, *scene_args)
+# Since we specified alpha as output channel, img has 4 channels now
+# We blend the image with a background image
+img = img[:, :, :3] * img[:, :, 3:4] + background * (1 - img[:, :, 3:4])
 
-    # Save the images.
-    # The output image is in the GPU memory if you are using GPU.
-    pyredner.imwrite(img, 'results/test_single_triangle_background/target.exr')
-    pyredner.imwrite(img, 'results/test_single_triangle_background/target.png')
+# Save the images.
+# The output image is in the GPU memory if you are using GPU.
+pyredner.imwrite(img, 'results/test_single_triangle_background/target.exr')
+pyredner.imwrite(img, 'results/test_single_triangle_background/target.png')
 
-
-    # Perturb the scene, this is our initial guess.
-    shape_triangle.vertices = tfe.Variable(
+# Perturb the scene, this is our initial guess.
+with tf.device(pyredner.get_device_name()):
+    shape_triangle.vertices = tf.Variable(
         [[-2.0,1.5,0.3], [0.9,1.2,-0.3], [-0.4,-1.4,0.2]],
         dtype=tf.float32,
-        trainable=True)
-    # We need to serialize the scene again to get the new arguments.
-    scene_args = pyredner.serialize_scene(
-        scene = scene,
-        num_samples = 16,
-        max_bounces = 1,
-        channels = [redner.channels.radiance, redner.channels.alpha])  # NOTE: 
+        trainable=True,
+        use_resource=True)
+# We need to serialize the scene again to get the new arguments.
+scene_args = pyredner.serialize_scene(
+    scene = scene,
+    num_samples = 16,
+    max_bounces = 1,
+    channels = [redner.channels.radiance, redner.channels.alpha])
 
-    # Render the initial guess.
-    img = pyredner.render(1, *scene_args)
-    # Blend the image with a background image
-    img = img[:, :, :3] * img[:, :, 3:4] + background * (1 - img[:, :, 3:4])
-    # Save the images.
-    pyredner.imwrite(img, 'results/test_single_triangle_background/init.png')
-    # Compute the difference and save the images.
-    diff = tf.abs(target - img)
-    pyredner.imwrite(diff, 'results/test_single_triangle_background/init_diff.png')
+# Render the initial guess.
+img = pyredner.render(1, *scene_args)
+# Blend the image with a background image
+img = img[:, :, :3] * img[:, :, 3:4] + background * (1 - img[:, :, 3:4])
+# Save the images.
+pyredner.imwrite(img, 'results/test_single_triangle_background/init.png')
+# Compute the difference and save the images.
+diff = tf.abs(target - img)
+pyredner.imwrite(diff, 'results/test_single_triangle_background/init_diff.png')
 
 # Optimize for triangle vertices.
 optimizer = tf.train.AdamOptimizer(5e-2)
@@ -155,12 +152,10 @@ for t in range(300):
 
     grads = tape.gradient(loss, [shape_triangle.vertices])
     print('grad:', grads)
-    optimizer.apply_gradients(
-        zip(grads, [shape_triangle.vertices])
-        )
+    optimizer.apply_gradients(zip(grads, [shape_triangle.vertices]))
 
     # Print the current three vertices.
-    # print('vertices:', shape_triangle.vertices)
+    print('vertices:', shape_triangle.vertices)
 
 # Render the final result.
 scene_args = pyredner.serialize_scene(
