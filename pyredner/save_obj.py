7e96db81da34577daa75a90048dd544ec2e5b78c
Tzu-Mao Li <tzumao@mit.edu>
2019-10-20 11:59:02 -0700

    fix save obj

--- pyredner/save_obj.py
+++ pyredner/save_obj.py
@@ -9,16 +9,16 @@ def save_obj(shape,
         uvs = shape.uvs.cpu() if shape.uvs is not None else None
         normals = shape.normals.cpu() if shape.normals is not None else None
         for i in range(vertices.shape[0]):
-            f.write('v {} {} {}'.format(vertices[i, 0], vertices[i, 1], vertices[i, 2]))
+            f.write('v {} {} {}\n'.format(vertices[i, 0], vertices[i, 1], vertices[i, 2]))
         if uvs is not None:
             for i in range(uvs.shape[0]):
                 if flip_tex_coords:
-                    f.write('vt {} {}'.format(uvs[i, 0], 1 - uvs[i, 1]))
+                    f.write('vt {} {}\n'.format(uvs[i, 0], 1 - uvs[i, 1]))
                 else:
-                    f.write('vt {} {}'.format(uvs[i, 0], uvs[i, 1]))
+                    f.write('vt {} {}\n'.format(uvs[i, 0], uvs[i, 1]))
         if normals is not None:
             for i in range(normals.shape[0]):
-                f.write('vn {} {} {}'.format(normals[i, 0], normals[i, 1], normals[i, 2]))
+                f.write('vn {} {} {}\n'.format(normals[i, 0], normals[i, 1], normals[i, 2]))
         indices = shape.indices.cpu()
         uv_indices = shape.uv_indices.cpu() if shape.uv_indices is not None else None
         normal_indices = shape.normal_indices.cpu() if shape.normal_indices is not None else None
@@ -39,17 +39,17 @@ def save_obj(shape,
                 else:
                     ni = ''
             if normals is not None:
-                f.write('f {}/{}/{} {}/{}/{} {}/{}/{}'.format(\
+                f.write('f {}/{}/{} {}/{}/{} {}/{}/{}\n'.format(\
                     vi[0], uvi[0], ni[0],
                     vi[1], uvi[1], ni[1],
                     vi[2], uvi[2], ni[2]))
             elif uvs is not None:
-                f.write('f {}/{} {}/{} {}/{}'.format(\
+                f.write('f {}/{} {}/{} {}/{}\n'.format(\
                     vi[0], uvi[0],
                     vi[1], uvi[1],
                     vi[2], uvi[2]))
             else:
-                f.write('f {} {} {}'.format(\
+                f.write('f {} {} {}\n'.format(\
                     vi[0],
                     vi[1],
                     vi[2]))
