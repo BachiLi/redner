5b1e867c14d2fa8866566415058a92a4fcf99cad
Tzu-Mao Li <tzumao@mit.edu>
2019-11-26 15:43:07 -0800

    migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,55 @@
-from distutils.core import setup
+# Taken from https://github.com/pybind/cmake_example/blob/master/setup.py
+import os
+import re
+import sys
+import platform
+import subprocess
 import importlib
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j8']
+        cmake_args += ['-DCMAKE_VERBOSE_MAKEFILE=on']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 torch_spec = importlib.util.find_spec("torch")
 tf_spec = importlib.util.find_spec("tensorflow")
@@ -14,5 +64,10 @@ if len(packages) == 0:
 
 setup(name = 'redner',
       version = '0.0.1',
-      description = 'Redner',
-      packages = packages)
+      description = 'A differentiable Monte Carlo ray tracer.',
+      author = 'Tzu-Mao Li',
+      license = 'MIT',
+      packages = packages,
+      ext_modules=[CMakeExtension('cmake_example')],
+      cmdclass=dict(build_ext=CMakeBuild),
+      zip_safe=False)
