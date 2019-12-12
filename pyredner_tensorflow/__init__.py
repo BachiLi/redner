19ae1fb6d05f31fb48602cd9ffbc32754e06aa8f
Tzu-Mao Li <tzumao@mit.edu>
2019-12-12 09:44:32 -0800

    cleanup tensorflow module

--- pyredner_tensorflow/__init__.py
+++ pyredner_tensorflow/__init__.py
@@ -1,5 +1,10 @@
-import numpy as np
 import tensorflow as tf
+try:
+  import redner
+except ImportError:
+  print("Warning: redner is not installed when you import pyredner_tensorflow."
+        " Please install redner, by following instructions at https://github.com/BachiLi/redner/wiki"
+        " or by using pip install redner-gpu or pip install redner.")
 from .device import *
 from .camera_type import *
 from .camera import *
@@ -21,7 +26,6 @@ from .render_utils import *
 from .render_tensorflow import *
 
 import os.path
-import redner
 from tensorflow.python.framework import ops
 
 if tf.__cxx11_abi_flag__ == 0:
@@ -30,35 +34,6 @@ else:
     assert(tf.__cxx11_abi_flag__ == 1)
     __data_ptr_module = tf.load_op_library(os.path.join(os.path.dirname(redner.__file__), 'libredner_tf_data_ptr_cxx11_abi.so'))
 
-DEBUG = False
-IS_UNIT_TEST = False
-
 def data_ptr(tensor):    
     addr_as_uint64 = __data_ptr_module.data_ptr(tensor)
     return int(addr_as_uint64)
-
-def write_tensor(path, tensor, height, width):
-    with open(path, 'w') as f:
-        for i in range(height):
-            for j in range(width):
-                f.write(f'{tensor[i,j]} ')
-            f.write('\n')
-
-def pretty_debug_print(grads, vars, iter_num=-1):
-    from pprint import pprint
-    print("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\")
-    if iter_num > -1:
-        print("Iteration: ", iter_num)
-    print(">>> GRADIENTS:")
-    if (isinstance(grads, dict)):
-        for k, v in grads.items():
-            print(k, v.shape, v.numpy())
-    elif (isinstance(grads, list)):
-        for k in grads:
-            print(k)
-    print("\n>>> VARIABLES:")
-    for v in vars:
-        print(v.name, v.shape, v.numpy())
-
-def get_render_args(seed, scene_args):
-    return [tf.constant(seed)] + scene_args
