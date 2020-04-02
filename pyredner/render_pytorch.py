fix camera distortion derivatives

pyredner/render_pytorch.py
@@ -704,9 +704,9 @@ class RenderFunction(torch.autograd.Function):
                 redner.float_ptr(buffers.d_cam_up.data_ptr()),
                 redner.float_ptr(0), # cam_to_world
                 redner.float_ptr(0), # world_to_cam
-                redner.float_ptr(buffers.d_distortion_params.data_ptr() if buffers.d_distortion_params is not None else 0),
                 redner.float_ptr(buffers.d_intrinsic_mat_inv.data_ptr()),
-                redner.float_ptr(buffers.d_intrinsic_mat.data_ptr()))
+                redner.float_ptr(buffers.d_intrinsic_mat.data_ptr()),
+                redner.float_ptr(buffers.d_distortion_params.data_ptr() if buffers.d_distortion_params is not None else 0))
         else:
             buffers.d_camera = redner.DCamera(\
                 redner.float_ptr(0), # pos
@@ -714,9 +714,9 @@ class RenderFunction(torch.autograd.Function):
                 redner.float_ptr(0), # up
                 redner.float_ptr(buffers.d_cam_to_world.data_ptr()),
                 redner.float_ptr(buffers.d_world_to_cam.data_ptr()),
-                redner.float_ptr(buffers.d_distortion_params.data_ptr() if buffers.d_distortion_params is not None else 0),
                 redner.float_ptr(buffers.d_intrinsic_mat_inv.data_ptr()),
-                redner.float_ptr(buffers.d_intrinsic_mat.data_ptr()))
+                redner.float_ptr(buffers.d_intrinsic_mat.data_ptr()),
+                redner.float_ptr(buffers.d_distortion_params.data_ptr() if buffers.d_distortion_params is not None else 0))
         buffers.d_vertices_list = []
         buffers.d_uvs_list = []
         buffers.d_normals_list = []

tests/test_camera_distortion.py
@@ -26,7 +26,7 @@ if pyredner.get_use_gpu():
     checkerboard_texture = checkerboard_texture.cuda(device = pyredner.get_device())
 
 mat_checkerboard = pyredner.Material(\
-    diffuse_reflectance = torch.tensor([0.5, 0.5, 0.5]))
+    diffuse_reflectance = checkerboard_texture)
 mat_black = pyredner.Material(\
     diffuse_reflectance = torch.tensor([0.0, 0.0, 0.0], device = pyredner.get_device()))
 

tests/test_texture.py
@@ -1,5 +1,4 @@
 import pyredner
-import numpy as np
 import torch
 
 # Optimize four vertices of a textured patch
@@ -24,7 +23,7 @@ cam = pyredner.Camera(position = position,
 
 checkerboard_texture = pyredner.imread('checkerboard.exr')
 if pyredner.get_use_gpu():
-	checkerboard_texture = checkerboard_texture.cuda(device = pyredner.get_device())
+    checkerboard_texture = checkerboard_texture.cuda(device = pyredner.get_device())
 
 mat_checkerboard = pyredner.Material(\
     diffuse_reflectance = checkerboard_texture)
@@ -37,7 +36,7 @@ vertices = torch.tensor([[-1.0,-1.0,0.0], [-1.0,1.0,0.0], [1.0,-1.0,0.0], [1.0,1
 indices = torch.tensor([[0, 1, 2], [1, 3, 2]], dtype = torch.int32,
                        device = pyredner.get_device())
 uvs = torch.tensor([[0.05, 0.05], [0.05, 0.95], [0.95, 0.05], [0.95, 0.95]],
-				   device = pyredner.get_device())
+                   device = pyredner.get_device())
 shape_plane = pyredner.Shape(vertices = vertices,
                              indices = indices,
                              uvs = uvs,