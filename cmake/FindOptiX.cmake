ef12a2a37361cedf7fc9cbb160f055bf8bae5b5b
Ahmet Levent Tasel <leventtasel@gmail.com>
2022-07-13 21:54:44 -0700

    update redner-dependencies submodule and add linux optix dependencies

--- cmake/FindOptiX.cmake
+++ cmake/FindOptiX.cmake
@@ -29,7 +29,7 @@
 # Locate the OptiX distribution.  Search relative to the SDK first, then look in the system.
 
 # Our initial guess will be within the SDK.
-set(OptiX_INSTALL_DIR "${CMAKE_SOURCE_DIR}/optix" CACHE PATH "Path to OptiX installed location.")
+set(OptiX_INSTALL_DIR "${CMAKE_SOURCE_DIR}/redner-dependencies/optix" CACHE PATH "Path to OptiX installed location.")
 
 # The distribution contains only 64 bit libraries.  Error when we have been mis-configured.
 if(NOT CMAKE_SIZEOF_VOID_P EQUAL 8)
